334cdfe329a65be9a9ae29a4f5582363a70746d6
jtewodros <67031628+jtewodros@users.noreply.github.com>
2020-07-08 14:45:46 -0400

    Add files via upload

---
+++ sudoku_solver.py
@@ -0,0 +1,91 @@
+# Jared Tewodros
+# jmt5rg
+
+# Jared Tewodros
+# jmt5rg
+from random import sample
+
+board = [
+        [0, 4, 0, 0, 0, 0, 0, 1, 0],
+        [2, 0, 0, 0, 0, 0, 0, 0, 6],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [9, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 1, 0, 0, 0, 0, 0, 9, 0]
+    ]
+
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            # reset last element because invalid config
+            board[row][col] = 0
+
+    return False
+
+
+def is_valid(board, number, position):
+    # check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    # check col
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # check square
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+
+    return True
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board)):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end='')
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                # row, col
+                return i, j
+
+    return None
+
+
+print_board(board)
+solve(board)
+print("################################")
+print_board(board)
